1a102c9416191547b8294c7d3d6d56f2d2156c65
Karen Calulo <kcalulo@gmail.com>
2022-03-08 20:39:04 +0800

    Modify NavBar UI

--- resources/views/partials/navBar.blade.php
+++ resources/views/partials/navBar.blade.php
@@ -1,24 +1,19 @@
 <!-- NAVIGATION BAR -->
-<nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #9f774d;">
+    <a class="navbar-brand" href="{{ route('dashboard')}}">{{
+        config('app.name')}}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{ route('dashboard') }}">Dashboard <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('animals.index')}}">Animals</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Rescuers</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="{{route('animals.index')}}">Animals<span
+                    class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="{{route('rescuers.index')}}">Rescuers</a>
+            <a class="nav-item nav-link" href="#">Employees</a>
+            <a class="nav-item nav-link" href="#">Adopters</a>
+            <a class="nav-item nav-link" href="#">Donations</a>
+        </div>
     </div>
 </nav>
