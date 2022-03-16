7f24b0348983aad941e0cfc58d2905fe7ebb50b9
Karen Calulo <kcalulo@gmail.com>
2022-03-16 22:38:44 +0800

    Modify NavBar Active Menu

--- resources/views/partials/navBar.blade.php
+++ resources/views/partials/navBar.blade.php
@@ -8,17 +8,22 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <a class="nav-item nav-link" href="{{route('animals.index')}}">Animals<span
-                    class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="{{route('rescuers.index')}}">Rescuers</a>
-            <a class="nav-item nav-link" href="{{route('employees.index')}}">Employees</a>
-            <a class="nav-item nav-link" href="#">Adopters</a>
+            <a class="nav-item nav-link {{(request()->is('animals*')) ? 'active' : ''}}"
+                href="{{route('animals.index')}}">Animals</a>
+            <a class="nav-item nav-link {{(request()->is('rescuers*')) ? 'active' : ''}}"
+                href="{{route('rescuers.index')}}">Rescuers</a>
+            <a class="nav-item nav-link {{(request()->is('employees*')) ? 'active' : ''}}"
+                href="{{route('employees.index')}}">Employees</a>
+            <a class="nav-item nav-link " href="#">Adopters</a>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false" href="#">Donations </a>
+                <a class="nav-link dropdown-toggle {{(request()->is('donations*')) ? 'active' : ''}}"
+                    id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                    href="#">Donations </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="{{route('donations.index', 'Cash')}}">Cash Donations</a>
-                    <a class="dropdown-item" href="{{route('donations.index', 'Items')}}">Items Donations</a>
+                    <a class="dropdown-item {{(request()->is('donations/Cash*')) ? 'active' : ''}}"
+                        href="{{route('donations.index', 'Cash')}}">Cash Donations</a>
+                    <a class="dropdown-item {{(request()->is('donations/Items*')) ? 'active' : ''}}"
+                        href="{{route('donations.index', 'Items')}}">Items Donations</a>
                 </div>
             </li>
         </div>
